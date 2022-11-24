9227c38bedc2bbc0a2579133da8c96c9feae2bf1
Gopal <82232811+gopal-ag@users.noreply.github.com>
2022-11-24 22:58:51 +0530

    Add files via upload

---
+++ Vichele Ditector copy/vehicle_counting.py
@@ -0,0 +1,34 @@
+import cv2
+import glob
+from vehicle_detector import VehicleDetector
+
+
+vd = VehicleDetector()
+
+images_folder = glob.glob("images/*.jpg")
+print(images_folder)
+
+vehicles_folder_count = 0
+
+
+for img_path in images_folder:
+    print("Img path", img_path)
+    img = cv2.imread(img_path)
+
+    vehicle_boxes = vd.detect_vehicles(img)
+    vehicle_count = len(vehicle_boxes)
+    print(vehicle_count)
+
+
+    vehicles_folder_count += vehicle_count
+
+    for box in vehicle_boxes:
+        x, y, w, h = box
+
+        cv2.rectangle(img, (x, y), (x + w, y + h), (25, 0, 180), 3)
+
+        cv2.putText(img, "Counted Vehicles:" + str(vehicle_count), (20, 50), 0, 2, (100, 200, 0), 3)
+
+    cv2.imshow("Cars", img)
+    cv2.waitKey(2)
+
